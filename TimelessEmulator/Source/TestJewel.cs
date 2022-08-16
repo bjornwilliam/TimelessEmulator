da530adea49dbf35313e2b0b9710d807ce7218bd
bjorn <nomail@mail.com>
2022-08-16 21:41:33 +0200

    hacked to generate glorious lookup data

--- TimelessEmulator/Source/TestJewel.cs
+++ TimelessEmulator/Source/TestJewel.cs
@@ -1,8 +1,14 @@
-﻿using Spectre.Console;
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using Spectre.Console;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+
 using System.Threading.Tasks;
 using TimelessEmulator.Data;
 using TimelessEmulator.Data.Models;
@@ -16,65 +22,110 @@ namespace TimelessEmulator
 
 
 
-    public void ProcessIds(List<string> ids)
+    public void ProcessIds(string socketName, List<string> ids,string savePath)
     {
             DataManager.Initialize();
 
             var xibaquaJewel = new TimelessJewelConqueror(1, 0);
             uint alternateTreeVersionIndex = 1;
             AlternateTreeVersion alternateTreeVersion = DataManager.AlternateTreeVersions.First(q => (q.Index == alternateTreeVersionIndex));
+            // Csv 
+            //var writer = new StreamWriter(savePath);
+            //var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            var records = new List<dynamic>();
 
 
-
-
-
-            var seedRange = (100, 8000);
-
-            var auraEffects = new Dictionary<int, uint>();
+            var listOfDicts = new List<Dictionary<string, dynamic>>();
+            //var auraEffects = new Dictionary<int, uint>();
             for (int i = 100; i <= 8000; i++)
             {
-                uint aura_effect = 0;
+
+                var jsonDict = new Dictionary<string, dynamic>();
+                jsonDict["seed"] = i;
+                jsonDict["socketName"] = socketName;
+                jsonDict["passiveChangesList"] = new List<dynamic>();
+                //record.Seed = i;
                 foreach (var id in ids)
                 {
+                    var passiveDict = new Dictionary<string, dynamic>();
+                    passiveDict["graphId"] = id;
+
+
+
                     var timelessJewel = new TimelessJewel(alternateTreeVersion, xibaquaJewel, (uint)i);
                     PassiveSkill passiveSkill = DataManager.GetPassiveSkillByFuzzyValue(id);
                     if (passiveSkill == null) continue;
                     AlternateTreeManager alternateTreeManager = new AlternateTreeManager(passiveSkill, timelessJewel);
-
                     bool isPassiveSkillReplaced = alternateTreeManager.IsPassiveSkillReplaced();
-                    if (isPassiveSkillReplaced == false) continue;
+                    //AnsiConsole.MarkupLine($"\t[purple]Passive[/]: [yellow]{passiveSkill.Name} {isPassiveSkillReplaced.ToString()}[/]");
+
+                    if (isPassiveSkillReplaced == false)
+                    {
+                        continue;
+                    }
                     if (passiveSkill.IsJewelSocket == true) continue;
                     AlternatePassiveSkillInformation alternatePassiveSkillInformation = alternateTreeManager.ReplacePassiveSkill();
 
-                    aura_effect += PrintAlternatePassiveAdditionInformations(alternatePassiveSkillInformation.AlternatePassiveAdditionInformations);
 
+                    var alternatePassiveDict = new Dictionary<string, dynamic>();
+                    for (int j = 0; j < alternatePassiveSkillInformation.AlternatePassiveSkill.StatIndices.Count; j++)
+                    {
+                        uint statIndex = alternatePassiveSkillInformation.AlternatePassiveSkill.StatIndices.ElementAt(j);
+                        uint statRoll = alternatePassiveSkillInformation.StatRolls.ElementAt(j).Value;
+
+                        string statText =  DataManager.GetStatTextByIndex(statIndex);
+                        //DataManager.GetStatIdentifierByIndex(statIndex)
+                        string statIdentifier = DataManager.GetStatIdentifierByIndex(statIndex);
+                        if (alternatePassiveDict.ContainsKey(statIdentifier))
+                        {
+                            alternatePassiveDict[statIdentifier] += statRoll;
+                        }
+                        else
+                        {
+                            alternatePassiveDict[statIdentifier] = statRoll;
+                        }
+                        //AnsiConsole.MarkupLine($"\t\tStat [yellow]{i}[/] | [yellow]{DataManager.GetStatTextByIndex(statIndex)}[/] (Identifier: [yellow]{DataManager.GetStatIdentifierByIndex(statIndex)}[/], Index: [yellow]{statIndex}[/]), Roll: [yellow]{statRoll}[/]");
+                    }
+                    passiveDict["alternate"] = alternatePassiveDict;
+
+                    var additionalPassiveDict = new Dictionary<string, dynamic>();
+                    foreach (AlternatePassiveAdditionInformation alternatePassiveAdditionInformation in alternatePassiveSkillInformation.AlternatePassiveAdditionInformations)
+                    {
+                        //AnsiConsole.MarkupLine($"\t[green]Addition[/]: [yellow]{alternatePassiveAdditionInformation.AlternatePassiveAddition.Identifier}[/]");
+                        for (int t = 0; t < alternatePassiveAdditionInformation.AlternatePassiveAddition.StatIndices.Count; t++)
+                        {
+                            uint statIndex = alternatePassiveAdditionInformation.AlternatePassiveAddition.StatIndices.ElementAt(t);
+                            uint statRoll = alternatePassiveAdditionInformation.StatRolls.ElementAt(t).Value;
+                            string statText = DataManager.GetStatTextByIndex(statIndex);
+                            string statIdentifier = DataManager.GetStatIdentifierByIndex(statIndex);
+                            if (additionalPassiveDict.ContainsKey(statIdentifier))
+                            {
+                                additionalPassiveDict[statIdentifier] += statRoll;
+                            }
+                            else
+                            {
+                                additionalPassiveDict[statIdentifier] = statRoll;
+                            }
+                            //AnsiConsole.MarkupLine($"\t\tStat [yellow]{i}[/] | [yellow]{DataManager.GetStatTextByIndex(statIndex)}[/] (Identifier: [yellow]{DataManager.GetStatIdentifierByIndex(statIndex)}[/], Index: [yellow]{statIndex}[/]), Roll: [yellow]{statRoll}[/]");
+                        }
+                    }
+                    passiveDict["additions"] = additionalPassiveDict;
+
+                    jsonDict["passiveChangesList"].Add(passiveDict);
                 }
-                auraEffects[i] = aura_effect;
-
+                listOfDicts.Add(jsonDict);
             }
-            var sortedDict = from entry in auraEffects orderby entry.Value descending select entry;
-
-
-
-            //var timelessJewel = new TimelessJewel(alternateTreeVersion, timelessJewelConqueror, timelessJewelSeed);
-
-            //AlternateTreeManager alternateTreeManager = new AlternateTreeManager(passiveSkill, timelessJewel);
-
-            //            bool isPassiveSkillReplaced = alternateTreeManager.IsPassiveSkillReplaced();
-
-            //AlternatePassiveSkillInformation alternatePassiveSkillInformation = alternateTreeManager.ReplacePassiveSkill();
-
-            //AnsiConsole.MarkupLine($"[green]Alternate Passive Skill[/]: [yellow]{alternatePassiveSkillInformation.AlternatePassiveSkill.Name}[/] ([yellow]{alternatePassiveSkillInformation.AlternatePassiveSkill.Identifier}[/])");
-
-            //for (int i = 0; i < alternatePassiveSkillInformation.AlternatePassiveSkill.StatIndices.Count; i++)
+            using (StreamWriter file = File.CreateText(savePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, listOfDicts);
+            }
+            //using (var writer = new StreamWriter(savePath))
+            //using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
             //{
-            //    uint statIndex = alternatePassiveSkillInformation.AlternatePassiveSkill.StatIndices.ElementAt(i);
-            //    uint statRoll = alternatePassiveSkillInformation.StatRolls.ElementAt(i).Value;
-
-            //    AnsiConsole.MarkupLine($"\t\tStat [yellow]{i}[/] | [yellow]{DataManager.GetStatTextByIndex(statIndex)}[/] (Identifier: [yellow]{DataManager.GetStatIdentifierByIndex(statIndex)}[/], Index: [yellow]{statIndex}[/]), Roll: [yellow]{statRoll}[/]");
+            //    csv.WriteRecords(records);
             //}
 
-            //PrintAlternatePassiveAdditionInformations(alternatePassiveSkillInformation.AlternatePassiveAdditionInformations);
         }
 
         private static uint PrintAlternatePassiveAdditionInformations(IReadOnlyCollection<AlternatePassiveAdditionInformation> alternatePassiveAdditionInformations)
@@ -93,8 +144,12 @@ namespace TimelessEmulator
                     uint statRoll = alternatePassiveAdditionInformation.StatRolls.ElementAt(i).Value;
                     if (statIndex == 3840)
                     {
+                        if (statRoll > 4)
+                        {
+                            var debug = 5;
+                        }
                         aura_effect += statRoll;
-                    }
+                    } 
 
                     //AnsiConsole.MarkupLine($"\t\tStat [yellow]{i}[/] | [yellow]{DataManager.GetStatTextByIndex(statIndex)}[/] (Identifier: [yellow]{DataManager.GetStatIdentifierByIndex(statIndex)}[/], Index: [yellow]{statIndex}[/]), Roll: [yellow]{statRoll}[/]");
                 }
